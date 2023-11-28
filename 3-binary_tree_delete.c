cb6c5cb38aa2251fc8ba1124750320c591f53c63
Gelfato Gebre <gelf.learing@gmail.com>
2023-11-28 18:44:32 +0300

    adding betty style of coding

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,13 +10,13 @@
  *
  * Return: Nothing
  */
- 
+
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree != NULL)
-    {
-        binary_tree_delete(tree->left);
-        binary_tree_delete(tree->right);
-        free(tree);
-    }
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
 }
